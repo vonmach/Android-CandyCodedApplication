6cea1972376a652252ae6ec780db01d8c86183fb
vonmach <48648606+vonmach@users.noreply.github.com>
2019-10-26 12:33:56 +0200

    Update InfoActivity.java

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -29,7 +29,7 @@ public class InfoActivity extends AppCompatActivity {
     // TODO - Task 2 - Launch the Google Maps Activity
     // ***
     public void createMapIntent(View view){
-        Uri uriAddress = Uri.parse("geo:0.07q=618 E South St Orlando, FL 32801");
+        Uri uriAddress = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, uriAddress);
         mapIntent.setPackage("com.google.android.apps.maps");
 
